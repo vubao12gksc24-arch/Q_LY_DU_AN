06f3a876bd3e403d9c71851131ee757ee470b776
Vu Bao <emailcuaban@gmail.com>
2026-03-26 09:50:08 +0700

    create env example

---
+++ config/env.example.php
@@ -0,0 +1,14 @@
+<?php
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL', 'http://du_an_1.test/');
+define('UPLOADS_URL', BASE_URL . 'uploads/');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'database');  // Tên database
+
+define('PATH_ROOT', __DIR__ . '/../');
