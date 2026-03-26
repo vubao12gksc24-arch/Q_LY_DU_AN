f43d33b221e723bfec540550c8177968b2d2e0c2
Vu Bao <emailcuaban@gmail.com>
2026-03-26 09:33:39 +0700

    them router index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once './config/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+require_once './commons/helperTree.php'; // Hàm hỗ trợ
+require_once './commons/message.php'; // Hàm hỗ trợ
+require_once './config/autoload.php'; //Tự động require Controller và Models
+//Router
+require_once './routers/web.php';
+// echo "<?php // test ?>" > routes/index.php;
